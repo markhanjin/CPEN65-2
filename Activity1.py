afb7a47a9da9999593b5273619c03c74e8695555
Mark Hanjin Valenzuela <90833252+markhanjin@users.noreply.github.com>
2022-10-12 19:48:01 +0800

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,8 @@
+#Create a Python program that computes the area of a circle
+#Area = pi * r^2
+pi = 3.14
+r = float(input("Enter the radius of the circle:"))
+Area = pi*(r*r)
+print("The area of the circle is" + " " + str(Area))
+
+
